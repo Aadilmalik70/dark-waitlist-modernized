2372ebd8633904b48d92db15f36441944c94b307
Aadilkhan-og <aadil.outworktech@gmail.com>
2026-06-08 18:45:00 +0530

    fix(seo): Fix 404 error on /blog/category/digital-marketing

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ module.exports = {
         source: '/blog/create-content-that-dominates-search',
         destination: '/404',
         permanent: false
+      },
+      {
+        source: '/blog/category/digital-marketing',
+        destination: '/',
+        permanent: true
       }
     ]
   }
